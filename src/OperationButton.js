5b0c49c888409e01df233514f141886d4f27a441
bhargavi <122342419+bhxrg@users.noreply.github.com>
2024-01-20 16:10:58 +0530

    Add files via upload

---
+++ src/OperationButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App";
+import DigitButton from "./DigitButton";
+
+export default function OperationButton({dispatch,operation}) {
+    return (
+        <button
+        onClick={()=>dispatch({ type: ACTIONS.CHOOSE_OPERATION,payload : { operation } })}
+        >
+        {operation}
+        </button>
+    )
+}
